434c53220553d731fd440c1e5bf2ac6c264f040b
Tanke <albertocantabria@gmail.com>
2019-05-25 14:27:53 +0200

    commit inicial de proyecto simple a desplegar en aws

---
+++ albertoweb/src/es/alberto/albertoweb/servlet/ServletLogin.java
@@ -0,0 +1,52 @@
+package es.alberto.albertoweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet(description = "Servlet para parametrizar el login", urlPatterns = { "/ServletLogin" })
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String usuario = request.getParameter("usuario");
+		String pass = request.getParameter("pass");
+		PrintWriter escritor = response.getWriter();
+		if (usuario!=null && pass!=null) {
+			if(usuario.equals("Alberto")) {
+				escritor.println("Enorme Alberto");
+			}else 
+				escritor.println("y tu quien eres comepinga");
+		}else {
+			escritor.println("Usuario incorrecto!!");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
